367d0c5c0245516f92dc03ace31213c46678e6e6
Gustav Simonsson <gustav.simonsson@gmail.com>
2015-05-07 04:30:07 +0200

    Dev commit. Squash this commit in later rebase

--- core/chain_manager.go
+++ core/chain_manager.go
@@ -282,7 +282,7 @@ func (bc *ChainManager) NewBlock(coinbase common.Address) *types.Block {
 
 		// fork it baby
 		pushItToTheLimit := new(big.Int).Sub(parent.GasLimit(), new(big.Int).Div(parent.GasLimit(), big.NewInt(1024)))
-		pushItToTheLimit = new(big.Int).Add(pushItToTheLimit, big.NewInt(1))
+		pushItToTheLimit = new(big.Int).Add(pushItToTheLimit, big.NewInt(0))
 		//fmt.Println("HURR: parent num:      ", parent.NumberU64())
 		//fmt.Println("HURR: parent limit:    ", parent.GasLimit())
 		fmt.Println("HURR: attempted limit: ", pushItToTheLimit)
